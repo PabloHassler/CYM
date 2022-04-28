7fb3acf1a1868eb60d0d7bff2df0c0d37cf9b7c0
pablo.h <88948970+PabloHassler@users.noreply.github.com>
2022-04-28 13:29:21 -0300

    MEF del reloj
    falta implementar una funcion actualizarCampo(), todo lo agregado no esta probado

--- Reloj/Reloj/main.c
+++ Reloj/Reloj/main.c
@@ -6,49 +6,67 @@
  */  
 
 /* Inclusión de cabeceras de bibliotecas de código */
-#include <avr/io.h> // Definición de Registros del microcontrolador
-#define F_CPU 16000000UL // Especifico la frecuencia de reloj del MCU en 8MHz
-#include <util/delay.h> // Retardos por software – Macros: depende de F_CPU
+
+#include <avr/io.h>			// Definición de Registros del microcontrolador
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include "lcd.h"
+#define DIA=4;
+#define MES=7;
+#define ANO=10;
+#define HORA=4;
+#define MINUTO=7;
+#define SEGUNDO=10;
+
 uint8_t KEYPAD_scan (uint8_t *);
 uint8_t KEYPAD_Update (uint8_t *);
-void setup();
+void setupLCD();
+void setupTimer();
+void actualizar_MEF();
+void iniciar_MEF();
+static char not_leap(void);
+void salida(uint8_t,uint8_t,uint8_t);
+void actualizarTiempo(); 
+void actualizarCampo(char,uint8_t);
+void imprimir();
 
-ISR(TIMER0_OVF_vect)
-{
-	static unsigned cont=0;
-	static unsigned seg=0;
-	if(++cont==50){
-		//
-		seg++;
-		TCNT0=99;//reinicio contador del timer0
-		cont=0;//reinicio contador
-	}
-	
-}
+typedef enum{S0,S1,S2,S3,S4,S5,S6} state;
+state estado;state estadoAuxiliar;
+
+typedef struct{
+	unsigned char second;
+	unsigned char minute;
+	unsigned char hour;
+	unsigned char date;
+	unsigned char month;
+	unsigned char year;
+}time;
+
+volatile time t={10,29,14,19,4,21};
+volatile time t_Parcial={10,29,14,19,4,21};
+volatile uint8_t FlagCursor=0;
+const uint8_t filas[4] =   {0b00010000,0b00001000,0b00000001,0b10000000};
+const uint8_t columna[4] = {0b00001000,0b00100000,0b00010000,0b00000100};
+const char codChar[4][4] = {{'1','2','3','A'},
+{'4','5','6','B'},
+{'7','8','9','C'},
+{'0','*','#','D'}};
 
 int main(void)
 {
     /* Replace with your application code */
-   uint8_t key=0x00;
-	TCCR0B=(1<<CS02)|(1<<CS00);//configurar el registro del timer0 como temporizador con prescalador de 1024
-	TCNT0=99;//el registro empieza con valor 99
-	TIMSK0|= (1<<TOIE0);//habilita la interrupcion por desbordamiento del timer0
-	sei();//habilita interrupciones globales
-	setup();
-
+	setupTimer();
+	setupLCD();
+	
+	iniciar_MEF();
 	while (1) 
     {
-		if(KEYPAD_Update (&key)){
-			LCDsendChar(key);//se manda key al lcd
-			LCDGotoXY(6,1);
-			_delay_ms(5);
-		}
+		actualizar_MEF();	
     }
 }
+
 /*SETUP DE LA PANTALLA LCD*/
-void setup(){
+void setupLCD(){
 	LCDinit();
 	LCDclr();
 	LCDGotoXY(0,0);
@@ -56,6 +74,198 @@ void setup(){
 	LCDGotoXY(0,1);
 	LCDstring((uint8_t*)"23:59:59",8);
 }
+
+void setupTimer(){
+	DDRC = 0xFF;											//Configure all eight pins of port B as outputs
+	TIMSK2 &= ~((1<<TOIE2)|(1<<OCIE2A));					//Make sure all TC0 interrupts are disabled
+	ASSR |= (1<<AS2);										//set Timer/counter0 to be asynchronous from the CPU clock
+															//with a second external clock (32,768kHz)driving it.
+	TCNT2 =0;												//Reset timer
+	TCCR2B =(1<<CS20)|(1<<CS22);							//Prescale the timer to be clock source/128 to make it
+															//exactly 1 second for every overflow to occur
+	while (ASSR & ((1<<TCN2UB)|(1<<OCR2AUB)|(1<<TCR2AUB)))	//Wait until TC0 is updated
+	{}
+	TIMSK2 |= (1<<TOIE2);									//Set 8-bit Timer/Counter0 Overflow Interrupt Enable
+	sei();
+}
+
+ISR(TIMER2_OVF_vect)
+{
+	static uint8_t cambio=0;
+	if(FlagCursor){
+		if(cambio){
+			LCDcursorOn();
+		}
+		else{
+			LCDcursorOFF();
+		}
+		cambio=!cambio;
+	}
+	if (++t.second==60)        //keep track of time, date, month, and year
+	{
+		t.second=0;
+		if (++t.minute==60)
+		{
+			t.minute=0;
+			if (++t.hour==24)
+			{
+				t.hour=0;
+				if (++t.date==32)
+				{
+					t.month++;
+					t.date=1;
+				}
+				else if (t.date==31)
+				{
+					if ((t.month==4) || (t.month==6) || (t.month==9) || (t.month==11))
+					{
+						t.month++;
+						t.date=1;
+					}
+				}
+				else if (t.date==30)
+				{
+					if(t.month==2)
+					{
+						t.month++;
+						t.date=1;
+					}
+				}
+				else if (t.date==29)
+				{
+					if((t.month==2) && (not_leap()))
+					{
+						t.month++;
+						t.date=1;
+					}
+				}
+				if (t.month==13)
+				{
+					t.month=1;
+					t.year++;
+				}
+			}
+		}
+	}
+}
+
+static char not_leap(void)      //check for leap year
+{
+	if (!(t.year%100))
+	{
+		return (char)(t.year%400);
+	}
+	else
+	{
+		return (char)(t.year%4);
+	}
+}
+
+void iniciar_MEF(){
+	estado=S0;
+	
+}
+
+void actualizar_MEF(){
+	uint8_t key=0x00;
+	static uint8_t nuevo=0;
+	nuevo=KEYPAD_Update (&key);
+	switch (estado){
+		case S0:
+			switch (key){
+				case 'A': estado=S1; salida(1,0,ANO);
+				case 'D': estado=S0; salida(0,0,ANO);
+				default: actualizarTiempo();
+			}
+		case S1:
+			switch (key){
+				case 'A': estado=S2; salida(1,0,MES);
+				case 'D': estado=S0; salida(0,0,MES);
+				case 'B': actualizarCampo('A',1); imprimir();
+				case 'C': actualizarCampo('A',0); imprimir();
+			}
+		case S2:
+			switch (key){
+				case 'A': estado=S3; salida(1,0,DIA);
+				case 'D': estado=S0; salida(0,0,DIA);
+				case 'B': actualizarCampo('M',1); imprimir();
+				case 'C': actualizarCampo('M',0); imprimir();
+		}
+		case S3:
+			switch (key){
+				case 'A': estado=S4; salida(1,0,HORA);
+				case 'D': estado=S0; salida(0,0,HORA);
+				case 'B': actualizarCampo('D',1); imprimir();
+				case 'C': actualizarCampo('D',0); imprimir();
+			}
+		case S4:
+			switch (key){
+				case 'A': estado=S5; salida(1,1,MINUTO);
+				case 'D': estado=S0; salida(0,1,MINUTO);
+				case 'B': actualizarCampo('h',1); imprimir();
+				case 'C': actualizarCampo('h',0); imprimir();
+			}
+		case S5:
+			switch (key){
+				case 'A': estado=S6; salida(1,1,SEGUNDO);
+				case 'D': estado=S0; salida(0,1,SEGUNDO);
+				case 'B': actualizarCampo('m',1); imprimir();
+				case 'C': actualizarCampo('m',0); imprimir();
+			}
+		case S6:
+			switch (key){
+				case 'A': estado=S0; salida(1,1,SEGUNDO); salida(0,1,SEGUNDO);
+				case 'D': estado=S0; salida(0,1,SEGUNDO);
+				case 'B': actualizarCampo('s',1); imprimir();
+				case 'C': actualizarCampo('s',0); imprimir();
+			}
+	}
+}
+
+void ActualizarTiempo(){
+	LCDGotoXY(4,0);
+	LCDescribeDato(t.date,2);
+	LCDsendChar('/');
+	LCDescribeDato(t.month,2);
+	LCDsendChar('/');
+	LCDescribeDato(t.year,2);
+	LCDGotoXY(4,1);
+	LCDescribeDato(t.hour,2);
+	LCDsendChar(':');
+	LCDescribeDato(t.minute,2);
+	LCDsendChar(':');
+	LCDescribeDato(t.second,2);
+}
+
+void imprimir(){
+	LCDGotoXY(4,0);
+	LCDescribeDato(t_Parcial.date,2);
+	LCDsendChar('/');
+	LCDescribeDato(t_Parcial.month,2);
+	LCDsendChar('/');
+	LCDescribeDato(t_Parcial.year,2);
+	LCDGotoXY(4,1);
+	LCDescribeDato(t_Parcial.hour,2);
+	LCDsendChar(':');
+	LCDescribeDato(t_Parcial.minute,2);
+	LCDsendChar(':');
+	LCDescribeDato(t_Parcial.second,2);
+}
+
+void salida(uint8_t z,uint8_t pos,uint8_t campo){
+	if(z=1){
+		t=t_Parcial;
+		LCDGotoXY(campo,pos);
+		FlagCursor=1;
+	}
+	else{
+		t_Parcial=t;
+		LCDGotoXY(campo,pos);
+		FlagCursor=0;
+	}
+}
+
+
 /********************************************************
 FUNCION PARA ESCANEAR UN TECLADO MATRICIAL Y DEVOLVER LA
 TECLA PRESIONADA UNA SOLA VEZ. TIENE DOBLE VERIFICACION Y
@@ -64,8 +274,7 @@ DEVUELVE:
 0 -> NO HAYNUEVA TECLA PRESIONADA
 1 -> HAY NUEVA TECLA PRESIONADA Y ES *pkey
 ********************************************************/
-uint8_t KEYPAD_Update (uint8_t *pkey)
-{
+uint8_t KEYPAD_Update (uint8_t *pkey){
 	static uint8_t Old_key;
 	uint8_t Key, Last_valid_key=0xFF; // no hay tecla presionada
 	if(!KEYPAD_scan(&Key)) {
@@ -83,13 +292,6 @@ uint8_t KEYPAD_Update (uint8_t *pkey)
 	Old_key=Key; //1era verificación
 	return 0;
 }
-
-const uint8_t filas[4] =   {0b00010000,0b00001000,0b00000001,0b10000000};
-const uint8_t columna[4] = {0b00001000,0b00100000,0b00010000,0b00000100};
-const char codChar[4][4] = {{'1','2','3','A'},
-							{'4','5','6','B'},
-							{'7','8','9','C'},
-							{'0','*','#','D'}};
 
 uint8_t KEYPAD_scan (uint8_t *key){
 	PORTD=0b01111111;
